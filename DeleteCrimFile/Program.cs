b776da88302828eee7e2b5720258a2082fc60fbf
Lucas de Jesus Sampaio <lucas.sampaio@br.atento.com>
2021-09-24 13:13:22 -0300

    First commit

---
+++ DeleteCrimFile/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DeleteCrimFile
+{
+    class Program
+    {
+        private readonly static string logFileName = $"{DateTime.Now:yyyy_MM_dd_hh_mm_ss}.txt";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                foreach (var file in File.ReadAllLines(args[0]))
+                {
+                    if (File.Exists(file))
+                    {
+                        string fileName = new FileInfo(file).Name;
+
+                        if (MoveAsync(file, $"{args[1]}/{fileName}").GetAwaiter().GetResult())
+                            RegisterLog(args[0].Replace(new FileInfo(args[0]).Name, ""),
+                                $"---------------------------\nArquivo ({fileName}) movido " +
+                                $"DE: {file} " +
+                                $"PARA: {args[1]}\\{fileName}\n");
+                        else
+                            RegisterLog(args[0].Replace(new FileInfo(args[0]).Name, ""),
+                                $"---------------------------\nErro ao mover arquivo - \n{fileName}\n");
+                    }
+                }
+
+                Console.WriteLine("PROCESSO CONCLUÍDO");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERRO NA EXECUÇÃO: {ex.Message}");
+                Console.ReadKey();
+            }
+        }
+
+        private static async Task<bool> MoveAsync(string oldFile, string newFile)
+        {
+            try
+            {
+                using (FileStream dfStream = File.Open(oldFile, FileMode.Open))
+                using (FileStream newStream = File.Create(newFile))
+                    await dfStream.CopyToAsync(newStream);
+
+                if (new FileInfo(oldFile).Length != new FileInfo(newFile).Length)
+                {
+                    try { File.Delete(newFile); } catch { }
+                    return false;
+                }
+                else 
+                {
+                    try { File.Delete(oldFile); } catch { }
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private static void RegisterLog(string path, string register)
+        {
+            if (!File.Exists($"{path}/{logFileName}"))
+                File.Create($"{path}/{logFileName}").Close();
+
+            File.AppendAllTextAsync($"{path}/{logFileName}", register).Dispose();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+To use the solution, open it in visual studio, add arguments in debug options!
+
+In first argument put the path of txt file whos contains all files (one by row),
+In the secund, include the new path to send your files (The solution need this arugment to make a backup). 
+	If you dont need, delete it finish 
+
+Has a log file, it created in same folder located under your first arg
